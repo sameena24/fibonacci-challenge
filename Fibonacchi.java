4fc6d9a744d55702ab3d58942509179660d57a8d
sameena24 <82368219+sameena24@users.noreply.github.com>
2021-04-24 10:37:48 +0530

    Create Fibonacchi.java
    it prints the series of fibonacchi series of numbers

---
+++ Fibonacchi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Fibonacchi
+{
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter number of terms");
+	int n=sc.nextInt();
+	int first=0,second=1,next;
+	System.out.println("Fibonacci series is ");
+	int i=0;
+	while(i<n)
+	{
+	if(i<=1)
+	    next=i;
+	else
+	{
+	   next=first+second;
+	   first=second;
+	   second=next;
+	}
+	System.out.println(next);
+	i++;
+	}
+	}
+}
